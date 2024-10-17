04ceaf0e32fd270047b2d9e2bdeeef2a32c0f45f
Javier Bonet <javbonet@gmail.com>
2024-10-17 20:00:39 -0300

    Adds logs to debug

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,6 +5,8 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
+console.log(process.env.NODE_ENV);
+console.log(isDevelopment);
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
