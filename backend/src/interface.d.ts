e11ac37a642fba54e839b161e985711ce0a85e19
Javier Bonet <javbonet@gmail.com>
2024-10-18 19:55:08 -0300

    Tries only exporting JWT from common interfaces

--- backend/src/interface.d.ts
+++ backend/src/interface.d.ts
@@ -1,8 +1,1 @@
-import { JwtPayload } from "jsonwebtoken";
-
-declare module JWT {
-  export interface Payload extends JwtPayload {
-    email: string;
-    role: string;
-  }
-}
+export { JWT } from "../../common/interfaces";
