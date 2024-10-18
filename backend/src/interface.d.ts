d903a3e5f0ec2d92a31d863d3953d90d2868763b
Javier Bonet <javbonet@gmail.com>
2024-10-18 19:38:52 -0300

    Adds JWT namespace declaration in backend interface file

--- backend/src/interface.d.ts
+++ backend/src/interface.d.ts
@@ -1,1 +1,10 @@
 export * from "../../common/interfaces";
+
+declare global {
+  namespace JWT {
+    export interface Payload extends JwtPayload {
+      email: string;
+      role: string;
+    }
+  }
+}
