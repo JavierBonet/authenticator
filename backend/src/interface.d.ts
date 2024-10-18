d15f2190af146b5d4d1f3dbb26d2792332abf21e
Javier Bonet <javbonet@gmail.com>
2024-10-18 19:50:01 -0300

    Adds explicit declaration for JWT

--- backend/src/interface.d.ts
+++ backend/src/interface.d.ts
@@ -1,3 +1,10 @@
 import { JwtPayload } from "jsonwebtoken";
 
 export * from "../../common/interfaces";
+
+declare module JWT {
+  export interface Payload extends JwtPayload {
+    email: string;
+    role: string;
+  }
+}
